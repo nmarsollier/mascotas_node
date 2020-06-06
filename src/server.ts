347ec04c281fc4cb293689a4cc84dc1e6023e6c1
nmarsollier <nmarsollier@gmail.com>
2020-06-06 16:08:02 -0300

    eslint

--- src/server.ts
+++ src/server.ts
@@ -14,20 +14,20 @@ mongoose.set("useUnifiedTopology", true);
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useCreateIndex", true);
 mongoose.connect(conf.mongoDb, {}, function (err: MongoError) {
-  if (err) {
-    console.error("No se pudo conectar a MongoDB!");
-    console.error(err.message);
-    process.exit();
-  } else {
-    console.log("MongoDB conectado.");
-  }
+    if (err) {
+        console.error("No se pudo conectar a MongoDB!");
+        console.error(err.message);
+        process.exit();
+    } else {
+        console.log("MongoDB conectado.");
+    }
 });
 
 // Se configura e inicia express
 const app = express.init(conf);
 
 app.listen(conf.port, () => {
-  console.log(`Mascotas escuchando en puerto ${conf.port}`);
+    console.log(`Mascotas escuchando en puerto ${conf.port}`);
 });
 
 module.exports = app;
